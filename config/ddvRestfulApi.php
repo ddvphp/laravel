3d13693fc4b238b8bb44f0d94aa0d60b93f2c6d9
hua <yuchonghua@163.com>
2017-07-03 16:25:20 +0800

    fixbug

--- config/ddvRestfulApi.php
+++ config/ddvRestfulApi.php
@@ -1,37 +1,18 @@
 <?php
 return array(
-    'headersPrefix' => 'x-hz-',
+    'headersPrefix' => 'x-ddv-',
     'cors' => array(// 来源
         'origin'=>array(
-            'http://10.8.8.23:3000',
             'http://127.0.0.1',
-            'http://10.8.8.88',
-            'http://10.8.8.35',
-            'http://10.8.8.7',
-            'http://10.8.8.23',
-            'http://10.8.7.240',
-            'http://10.8.7.192',
-            'http://127.0.0.1:8888',
-            'http://10.8.8.33:8888',
+            'http://127.0.0.1*',
+            'http://10.*.*.*',
+            'http://10.*.*.*:*'
         ),
         // 授权请求
         'method'=>array(
-            'GET',
-            'POST',
-            'PUT',
-            'HEAD',
-            'DELETE'
         ),
         // 授权请求头
         'allowHeader'=>array(
-            'accept',
-            'authorization',
-            'content-md5',
-            'content-type',
-            'x-requested-with',
-            'x_requested_with',
-            'cookie',
-            'x-hz-*'
         ),
         // 授权响应头读取
         'exposeHeader'=>array(
